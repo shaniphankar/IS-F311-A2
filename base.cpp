96f34c6b2ca4aa60808e78cdb6537f84779630ec
shaniphankar <shaniphankar@gmail.com>
2018-11-09 09:27:36 +0530

    Commented Somewhat

--- base.cpp
+++ base.cpp
@@ -3,11 +3,14 @@
 #include <glm/glm.hpp>
 #include <glm/vec3.hpp>
 using namespace std;
-double angle=0.0f,pitch=0.0f,yaw=-90.0f,fov=45.0f;
-glm::dvec3 cameraPos=glm::dvec3(0.0f,0.0f,4.0f);
-glm::dvec3 directionSight=glm::dvec3(0.0f,0.0f,-1.0f);
-glm::dvec3 upVec=glm::dvec3(0.0f,1.0f,0.0f);
-int flag=1;
+double pitch=0.0f;/*! The angle specifies the magnitude to which we are looking up or down. Calculated w.r.t x-axis */
+double yaw=-90.0f;/*! The angle specifies the magnitude to which we are looking left or right. Calculated w.r.t y-axis */
+double fov=45.0f;/*! This angle defines how much we can see of the scene.*/
+glm::dvec3 cameraPos=glm::dvec3(0.0f,0.0f,4.0f);/*! 3D vector that contains the position of camera i.e the PRP*/
+glm::dvec3 directionSight=glm::dvec3(0.0f,0.0f,-1.0f);/*! 3D vector that contains the direction of PRP w.r.t VRP*/
+glm::dvec3 upVec=glm::dvec3(0.0f,1.0f,0.0f);/*! 3D vector that contains the direction that defines what direction is UP*/
+
+/*! This function draws an empty Brown Cube which was the original prototype for our classroom*/
 void drawEmptyClass()
 {
 	glBegin(GL_QUADS);
@@ -81,7 +84,10 @@ void drawEmptyClass()
 	glVertex3f(1.0f,-1.0f,-1.0f);
 	glEnd();
 }
-
+/*! This function is the reshape function that is called everytime an OpenGL window is generated"
+\param w The width of the window.
+\param h The height of the window.
+*/
 void changeSize(int w, int h) 
 {
 	if (h == 0)
@@ -93,7 +99,8 @@ void changeSize(int w, int h)
 	gluPerspective(fov,ratio,1,100);
 	glMatrixMode(GL_MODELVIEW);
 }
-
+/*! This function is the display function that is registered with our OpenGL instance.This routine is redrawn everytime the OpenGL window is refreshed"
+*/
 void myDisplay(void) {
 
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -102,6 +109,8 @@ void myDisplay(void) {
 	drawEmptyClass();
     glutSwapBuffers();
 }
+/*! This function provides the initial settings for our OpenGL window"
+*/
 void myinit()
 {
 	glClearColor(0.0, 0.0, 0.0, 1.0);
@@ -110,11 +119,19 @@ void myinit()
 	glEnable(GL_DEPTH_TEST);
 	glDepthFunc(GL_LEQUAL);
 }
+
+/*! This function details the time intervals at which to redraw the scene in OpenGL"
+*/
 void update(int data)
 {
 	glutTimerFunc(45,update,0);
 	glutPostRedisplay();
 }
+/*! This function details how to process all the keys for which ASCII values exist"
+\param key The key that has been pressed.
+\param x Integer parameter
+\param y Integer parameter
+*/
 void processNormalKeys(unsigned char key, int x,int y)
 {
 	double sensitivityX=1.5f;
@@ -167,6 +184,11 @@ void processNormalKeys(unsigned char key, int x,int y)
 	directionSight=glm::normalize(tempDir);
 }
 
+/*! This function details how to process all the keys for which ASCII values do not exist"
+\param key The key that has been pressed.
+\param x Integer parameter
+\param y Integer parameter
+*/
 void processSpecialKeys(int key, int x,int y)
 {
 	double fraction=0.1f;
@@ -193,7 +215,7 @@ int main(int argc, char **argv)
 	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
 	glutInitWindowPosition(100,100);
 	glutInitWindowSize(1000,1000);
-	glutCreateWindow("Lighthouse3D - GLUT Tutorial");
+	glutCreateWindow("Classroom");
 	glutDisplayFunc(myDisplay);
 	glutReshapeFunc(changeSize);
 	glutKeyboardFunc(processNormalKeys);
