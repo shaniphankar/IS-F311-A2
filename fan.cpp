eef3312af117a309c8e19a29c0288b02c18c95af
Snigdha04 <snigdha0410@gmail.com>
2018-11-16 09:41:19 +0530

    Add files via upload

---
+++ fan.cpp
@@ -0,0 +1,248 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <math.h>
+#define GL_GLEXT_PROTOTYPES
+#ifdef __APPLE__
+#include <GLUT/glut.h>
+#else
+#include <GL/glut.h>
+#endif
+
+// ----------------------------------------------------------
+// Function Prototypes
+// ----------------------------------------------------------
+void display();
+void specialKeys();
+
+// ----------------------------------------------------------
+// Global Variables
+// ----------------------------------------------------------
+double rotate_y=0;
+double rotate_x=0;
+float _angle = -70.0f;
+// ----------------------------------------------------------
+// display() Callback function
+// ----------------------------------------------------------
+void display(){
+
+  //  Clear screen and Z-buffer
+  glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+
+  // Reset transformations
+  glLoadIdentity();
+
+  // Other Transformations
+  // glTranslatef( 0.4, 0.0, 0.0 );      // Not included
+  // glRotatef( 180, 0.0, 1.0, 0.0 );    // Not included
+
+  // Rotate when user changes rotate_x and rotate_y
+  glRotatef( rotate_x, 1.0, 0.0, 0.0 );
+  glRotatef( rotate_y, 0.0, 1.0, 0.0 );
+
+  // Other Transformations
+  // glScalef( 2.0, 2.0, 0.0 );          // Not included
+
+  glTranslatef( 0.0, 0.0, 0.4 );
+
+  //Multi-colored side - FRONT
+  glBegin(GL_POLYGON);
+  glColor3f(   1.0,  1.0, 0.0 );
+  glVertex3f(  0.03, -0.03, -0.4 );      // P1 is red
+  glVertex3f(  0.03,  0.03, -0.4 );      // P2 is green
+  glVertex3f( -0.03,  0.03, -0.4 );      // P3 is blue
+  glVertex3f( -0.03, -0.03, -0.4 );      // P4 is purple
+
+  glEnd();
+
+  // White side - BACK
+  glBegin(GL_POLYGON);
+  glColor3f(   1.0,  1.0, 1.0 );
+  glVertex3f(  0.03, -0.03, 0.4 );
+  glVertex3f(  0.03,  0.03, 0.4 );
+  glVertex3f( -0.03,  0.03, 0.4 );
+  glVertex3f( -0.03, -0.03, 0.4 );
+  glEnd();
+
+  // Purple side - RIGHT
+  glBegin(GL_POLYGON);
+  glColor3f(  1.0,  0.0,  1.0 );
+  glVertex3f( 0.03, -0.03, -0.4 );
+  glVertex3f( 0.03,  0.03, -0.4 );
+  glVertex3f( 0.03,  0.03,  0.4 );
+  glVertex3f( 0.03, -0.03,  0.4 );
+  glEnd();
+
+  // Green side - LEFT
+  glBegin(GL_POLYGON);
+  glColor3f(   0.0,  1.0,  0.0 );
+  glVertex3f( -0.03, -0.03,  0.4 );
+  glVertex3f( -0.03,  0.03,  0.4 );
+  glVertex3f( -0.03,  0.03, -0.4 );
+  glVertex3f( -0.03, -0.03, -0.4 );
+  glEnd();
+
+  // Blue side - TOP
+  glBegin(GL_POLYGON);
+  glColor3f(   0.0,  0.0,  1.0 );
+  glVertex3f(  0.03,  0.03,  0.4 );
+  glVertex3f(  0.03,  0.03, -0.4 );
+  glVertex3f( -0.03,  0.03, -0.4 );
+  glVertex3f( -0.03,  0.03,  0.4 );
+  glEnd();
+
+  // Red side - BOTTOM
+  glBegin(GL_POLYGON);
+  glColor3f(   1.0,  0.0,  0.0 );
+  glVertex3f(  0.03, -0.03, -0.4 );
+  glVertex3f(  0.03, -0.03,  0.4 );
+  glVertex3f( -0.03, -0.03,  0.4 );
+  glVertex3f( -0.03, -0.03, -0.4 );
+  glEnd();
+
+  glTranslatef( 0.0, 0.0, -0.4 );
+  glRotatef(_angle,0.0f, 0.0f, 1.0f);
+
+  //Multi-colored side - FRONT
+  glBegin(GL_POLYGON);
+  glColor3f(   1.0,  1.0, 0.0 );
+  glVertex3f(  0.12, -0.12, -0.05 );      // P1 is red
+  glVertex3f(  0.12,  0.12, -0.05 );      // P2 is green
+  glVertex3f( -0.12,  0.12, -0.05 );      // P3 is blue
+  glVertex3f( -0.12, -0.12, -0.05 );      // P4 is purple
+  glEnd();
+
+  // White side - BACK
+  glBegin(GL_POLYGON);
+  glColor3f(   1.0,  1.0, 1.0 );
+  glVertex3f(  0.12, -0.12, 0.05 );
+  glVertex3f(  0.12,  0.12, 0.05 );
+  glVertex3f( -0.12,  0.12, 0.05 );
+  glVertex3f( -0.12, -0.12, 0.05 );
+  glEnd();
+
+  // Purple side - RIGHT
+  glBegin(GL_POLYGON);
+  glColor3f(  1.0,  0.0,  1.0 );
+  glVertex3f( 0.12, -0.12, -0.05 );
+  glVertex3f( 0.12,  0.12, -0.05 );
+  glVertex3f( 0.12,  0.12,  0.05 );
+  glVertex3f( 0.12, -0.12,  0.05 );
+  glEnd();
+
+  // Green side - LEFT
+  glBegin(GL_POLYGON);
+  glColor3f(   0.0,  1.0,  0.0 );
+  glVertex3f( -0.12, -0.12,  0.05 );
+  glVertex3f( -0.12,  0.12,  0.05 );
+  glVertex3f( -0.12,  0.12, -0.05 );
+  glVertex3f( -0.12, -0.12, -0.05 );
+  glEnd();
+
+  // Blue side - TOP
+  glBegin(GL_POLYGON);
+  glColor3f(   0.0,  0.0,  1.0 );
+  glVertex3f(  0.12,  0.12,  0.05 );
+  glVertex3f(  0.12,  0.12, -0.05 );
+  glVertex3f( -0.12,  0.12, -0.05 );
+  glVertex3f( -0.12,  0.12,  0.05 );
+  glEnd();
+
+  // Red side - BOTTOM
+  glBegin(GL_POLYGON);
+  glColor3f(   1.0,  0.0,  0.0 );
+  glVertex3f(  0.12, -0.12, -0.05 );
+  glVertex3f(  0.12, -0.12,  0.05 );
+  glVertex3f( -0.12, -0.12,  0.05 );
+  glVertex3f( -0.12, -0.12, -0.05 );
+  glEnd();
+
+  glBegin(GL_TRIANGLES);
+  glColor3f( 1, 0, 0 ); // red
+  glVertex3f( -0.15, 0.9, 0 );
+  glVertex3f( 0.15, 0.9, 0 );
+  glVertex3f( 0, 0 , 0);
+  glEnd();
+
+  glRotatef( 120, 0.0, 0.0, 1.0 );
+  glBegin(GL_TRIANGLES);
+  glColor3f( 1, 0, 0 ); // red
+  glVertex3f( -0.15, 0.9, 0 );
+  glVertex3f( 0.15, 0.9, 0 );
+  glVertex3f( 0, 0 , 0);
+  glEnd();
+
+  glRotatef( 120, 0.0, 0.0, 1.0 );
+  glBegin(GL_TRIANGLES);
+  glColor3f( 1, 0, 0 ); // red
+  glVertex3f( -0.15, 0.9, 0 );
+  glVertex3f( 0.15, 0.9, 0 );
+  glVertex3f( 0, 0 , 0);
+  glEnd();
+
+
+  glFlush();
+  glutSwapBuffers();
+
+}
+
+void update(int value) {
+    _angle += 3.0f;
+    if (_angle > 360) {
+        _angle -= 360;
+    }
+    glutPostRedisplay();
+    glutTimerFunc(25, update, 0);
+}
+
+// ----------------------------------------------------------
+// specialKeys() Callback Function
+// ----------------------------------------------------------
+void specialKeys( int key, int x, int y ) {
+
+  //  Right arrow - increase rotation by 5 degree
+  if (key == GLUT_KEY_RIGHT)
+    rotate_y += 5;
+
+  //  Left arrow - decrease rotation by 5 degree
+  else if (key == GLUT_KEY_LEFT)
+    rotate_y -= 5;
+
+  else if (key == GLUT_KEY_UP)
+    rotate_x += 5;
+
+  else if (key == GLUT_KEY_DOWN)
+    rotate_x -= 5;
+
+  //  Request display update
+  glutPostRedisplay();
+
+}
+
+// ----------------------------------------------------------
+// main() function
+// ----------------------------------------------------------
+int main(int argc, char* argv[]){
+
+  //  Initialize GLUT and process user parameters
+  glutInit(&argc,argv);
+
+  //  Request double buffered true color window with Z-buffer
+  glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+
+  // Create window
+  glutCreateWindow("Awesome Cube");
+
+  //  Enable Z-buffer depth test
+  glEnable(GL_DEPTH_TEST);
+
+  // Callback functions
+  glutDisplayFunc(display);
+  glutSpecialFunc(specialKeys);
+  update(0);
+  //  Pass control to GLUT for events
+  glutMainLoop();
+
+  //  Return to OS
+  return 0;
+
+}
